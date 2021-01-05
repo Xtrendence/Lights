3fe790466adc9052045e8bc2dd288b2aba11af3f
Xtrendence <xtrendence@yahoo.com>
2021-01-05 23:33:46 +0000

    Added favicon.

--- public/index.php
+++ public/index.php
@@ -3,6 +3,15 @@
 	<head>
 		<link rel="stylesheet" href="./assets/css/style.css">
 		<link rel="stylesheet" href="./assets/css/resize.css">
+		<link rel="apple-touch-icon" sizes="180x180" href="./assets/img/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="./assets/img/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="./assets/img/favicon-16x16.png">
+		<link rel="manifest" href="./assets/img/site.webmanifest">
+		<link rel="mask-icon" href="./assets/img/safari-pinned-tab.svg" color="#5bbad5">
+		<link rel="shortcut icon" href="./assets/img/favicon.ico">
+		<meta name="msapplication-TileColor" content="#2d89ef">
+		<meta name="msapplication-config" content="./assets/img/browserconfig.xml">
+		<meta name="theme-color" content="#111111">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="mobile-web-app-capable" content="yes">
 		<meta charset="UTF-8">
